ceb7b2fc44df9634e85a91129e3573c2c9319bfe
SAOGonza <samuelortega86@yahoo.com>
2024-01-20 00:06:27 -0800

    Added ladder
    - Created LadderController script
    - Attached script to Player
    - Created ladder game object prefab
    - Applied pre-fab changes to Player game object.

---
+++ Assets/Ladder/LadderController.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LadderController : MonoBehaviour
+{
+    public Transform charController;
+    bool inside = false;
+    [SerializeField] private float speedUpDown = 10f;
+    public Movement playerInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerInput = GetComponent<Movement>();
+        inside = false;
+    }
+
+    private void OnTriggerEnter(Collider col)
+    {
+        // If we touch the ladder, disable character controls
+        // so they can move up and down the Y axis.
+        if (col.gameObject.tag == "Ladder")
+        {
+            playerInput.enabled = false;
+            inside = !inside;
+        }
+    }
+
+    private void OnTriggerExit(Collider col)
+    {
+        // We exited the ladder, give back player controls
+        // to move along the X axis.
+        if (col.gameObject.tag == "Ladder")
+        {
+            playerInput.enabled = true;
+            inside = !inside;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (inside && Input.GetKey("w"))
+        {
+            charController.transform.position += Vector3.up / speedUpDown;
+        }
+
+        if (inside && Input.GetKey("s"))
+        {
+            charController.transform.position += Vector3.down / speedUpDown;
+        }
+    }
+}
